3305ec875a9fb30656b97d6e784e8dc9eb89bf1e
Katie Wood <kwood6319@hotmail.co.uk>
2026-06-17 17:42:18 +0900

    Turn Controller OK

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,4 +1,38 @@
 class TurnsController < ApplicationController
   def create
+    @game  = Game.find(params[:game_id])
+    @team  = @game.teams.find(params[:team_id])
+    square = params[:square].to_i
+
+    record_turn(square)
+
+    if square == @game.goal_square
+      handle_win
+    else
+      handle_instruction(square)
+    end
+  end
+
+  private
+
+  def record_turn(square)
+    Turn.create(game: @game, team: @team, square: square)
+    @team.update(
+      position: square,
+      on_board: true,
+      turns_count: @team.turns_count + 1
+    )
+    @game.visit_square(square)
+  end
+
+  def handle_win
+    @game.update(status: "finished")
+    redirect_to @game, notice: "WIN:#{@team.name}"
+  end
+
+  def handle_instruction(square)
+    instruction = @game.random_instruction
+    @team.update(last_instruction: instruction)
+    redirect_to @game, notice: "INSTRUCTION:#{@team.color}:#{instruction}"
   end
 end
